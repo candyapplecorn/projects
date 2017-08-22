8ac7172f5b2ecbec0a203e1a8c45b68ea93d1de4
candyapplecorn <candyapplecorn@gmail.com>
2017-08-21 22:08:30 -0700

    finished phase 3: sharing artworks

--- W4D1/FirstRoutes/app/models/user.rb
+++ W4D1/FirstRoutes/app/models/user.rb
@@ -4,15 +4,17 @@ class User < ApplicationRecord
 	has_many :artworks,
 		primary_key: :id,
 		foreign_key: :artist_id,
-		class_name: 'Artwork'
+		class_name: 'Artwork',
+		dependent: :destroy
 
-	has_many :shares,
+	has_many :artwork_shares,
 		primary_key: :id,
 		foreign_key: :viewer_id,
-		class_name: 'ArtworkShare'
+		class_name: 'ArtworkShare',
+		dependent: :destroy
 
 	has_many :shared_artworks,
-		through: :shares,
+		through: :artwork_shares,
 		source: :artwork
 
 end
