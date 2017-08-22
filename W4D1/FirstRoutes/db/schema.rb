8ba19c8624c3e3ae670aed25cf555afd08afd5c2
candyapplecorn <candyapplecorn@gmail.com>
2017-08-21 20:54:54 -0700

    Fixed seeds.rb and finished creating correct migrations and associations for all 3 tables

--- W4D1/FirstRoutes/db/schema.rb
+++ W4D1/FirstRoutes/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822021326) do
+ActiveRecord::Schema.define(version: 20170822034104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,4 @@ ActiveRecord::Schema.define(version: 20170822021326) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  add_foreign_key "artwork_shares", "artworks"
-  add_foreign_key "artwork_shares", "users", column: "viewer_id"
 end
