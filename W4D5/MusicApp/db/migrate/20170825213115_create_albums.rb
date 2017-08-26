7263305fdf4a7d0cd1be04ef424aedf25dde75a8
Joseph Burger <candyapplecorn@gmail.com>
2017-08-25 17:19:13 -0700

    Worked up to phase 1: albums. Next step: tracks

---
+++ W4D5/MusicApp/db/migrate/20170825213115_create_albums.rb
@@ -0,0 +1,12 @@
+class CreateAlbums < ActiveRecord::Migration[5.1]
+  def change
+    create_table :albums do |t|
+      t.integer :band_id, null: false
+      t.string :title, null: false
+      t.date :year
+      t.boolean :live, default: false
+
+      t.timestamps
+    end
+  end
+end
