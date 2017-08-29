2a282766d114e2be617ed82e976b43e324fb08e7
Joseph Burger <candyapplecorn@gmail.com>
2017-08-28 17:25:24 -0700

    find_by_credentials

--- W5D1/GoalSetterApp/spec/models/user_spec.rb
+++ W5D1/GoalSetterApp/spec/models/user_spec.rb
@@ -6,24 +6,33 @@ RSpec.describe 'User', type: :model do
   let(:mary) { FactoryGirl.build(:no_username) }
   let(:nopass) { FactoryGirl.build(:no_password) }
 
-  it { should validate_presence_of(:email) }
+  let(:user) do
+    FactoryGirl.build(:user,
+      username: "jonathan@fakesite.com",
+      password: "good_password")
+  end
+
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
 
   it "creates a pass digest when given password" do
-    expect(user.password_digest).to_not be_nil
+    expect(gary.password_digest).to_not be_nil
   end
 
   it "creates a session token before validation" do
     gary.valid?
-    expect(user.session_token).to_not be_nil
+    expect(gary.session_token).to_not be_nil
   end
 
   describe ".find_by_credentials" do
-    before { gary.save! }
+    before { user.save! }
 
-    it "finds a username by credentials" do
-      expect(User.find_by_credentials(gary.username, gary.password).to eq(gary))
+    it "returns user given good credentials" do
+      expect(User.find_by_credentials(username: "jonathan@fakesite.com", password: "good_password")).to eq(user)
+    end
+
+    it "returns nil given bad credentials" do
+      expect(User.find_by_credentials(username: "jonathan@fakesite.com", password: "bad_password")).to eq(nil)
     end
   end
 end
